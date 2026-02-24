3b781ed0dc38188749dcb780b2f68a66c61c12b9
kondwanibmtawali <kondwanibmtawali@gmail.com>
2026-02-24 07:07:58 -0600

    Profile Image Change

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import "../assets/styles/Main.scss";
-import ProfileIMG from "../assets/images/ProfileIMG.png";
+import ProfessionalProfilePic from "../assets/images/ProfessionalProfilePic.png";
 
 function Main() {
 
@@ -10,7 +10,7 @@ function Main() {
         <div className="container">
             <div className="about-section">
                 <div className="image-wrapper">
-                    <img src={ProfileIMG} alt="Avatar" />
+                    <img src={ProfessionalProfilePic} alt="Avatar" />
                 </div>
                 <div className="content">
                     <div className="social_icons">
